e465f4ae570119dee6024269d26fae7628b26912
ItsNorin <itsnorin@gmail.com>
2019-09-29 17:21:42 -0700

    There, happy bicobus?

--- MineField.java
+++ MineField.java
@@ -1,65 +1,60 @@
 package main;
 
+import java.awt.Point;
+import java.util.ArrayList;
 import java.util.Random;
 import java.util.Scanner;
 
 public class MineField {
+	public static final String EMOTE_NUMBERS[] = { ":zero:", ":one:", ":two:", ":three:", ":four:", ":five:", ":six:",
+			":seven:", ":eight:" };
+	public static final String BOMB = ":bomb:";
+	public static final String SPOILER = "||";
 
-	public static int[][] setMines(int size, int noOfMines) {
+	protected static String getCellString(ArrayList<Point> field, Point p) {
+		if (field.contains(p))
+			return BOMB;
+
+		int bombCount = 0;
+		for (int i = -1; i <= 1; i++)
+			for (int j = -1; j <= 1; j++)
+				if (field.contains(new Point(p.x + i, p.y + j)))
+					bombCount++;
+		return EMOTE_NUMBERS[bombCount];
+	}
+
+	public static String getNewMineField(int size, int noOfMines) {
+		String result = "";
 		Random rand = new Random();
+		Point start = new Point(0,0);
+		ArrayList<Point> field = new ArrayList<Point>();
 
-		int mineLand[][] = new int[size][size];
-
-		for (int x = 0; x < size; x++)
-			for (int y = 0; y < size; y++)
-				mineLand[x][y] = 0;
-
+		// set mines
 		for (int i = 0; i < noOfMines; i++) {
-			int x = rand.nextInt(size);
-			int y = rand.nextInt(size);
-			if (mineLand[x][y] != -1)
-				mineLand[x][y] = -1; // -1 == bomb
+			Point p = new Point(rand.nextInt(size), rand.nextInt(size));
+			if (!field.contains(p))
+				field.add(p);
 			else
 				i--;
 		}
-
-		for (int x = 0; x < size; x++)
-			for (int y = 0; y < size; y++)
-				if (mineLand[x][y] == -1)
-					for (int i = -1; i <= 1; i++)
-						for (int j = -1; j <= 1; j++) 
-							try {
-								if (mineLand[x + i][y + j] != -1) 
-									mineLand[x + i][y + j]++;
-							} catch (Exception e) {}
-
-		return mineLand;
-	}
-
-	public static String toString(int mineField[][]) {
-		String result = "";
-		Random rand = new Random();
-
-		final String emoteNumbers[] = {":zero:",":one:", ":two:", ":three:", ":four:", ":five:", ":six:", ":seven:", ":eight:"};
-		final String bomb = ":bomb:";
-		final String spoiler = "||";
 		
-		int startX, startY;
-		
+		// find starting position
 		do {
-			startX = rand.nextInt(mineField.length);
-			startY = rand.nextInt(mineField.length);
-		} while(mineField[startX][startY] != 0);
+			start.x = rand.nextInt(size);
+			start.y = rand.nextInt(size);
+		} while (field.contains(start));
 		
-		for(int x = 0; x < mineField.length; x++) {
-			for(int y = 0; y < mineField[x].length; y++) {
-				if(x == startX && y == startY)
-					result += ((mineField[x][y] == -1) ? bomb : emoteNumbers[mineField[x][y]]) + " ";
-				else
-					result += spoiler + ((mineField[x][y] == -1) ? bomb : emoteNumbers[mineField[x][y]]) + spoiler + " ";
+		// create string
+		for(int x = 0; x < size; x++) {
+			for(int y = 0; y < size; y++) {
+				String cellStr = getCellString(field, new Point(x,y));
+				if(x != start.x && y != start.y) 
+					cellStr = SPOILER + cellStr + SPOILER + " ";
+				result += cellStr;	
 			}
 			result += "\n";
 		}
+
 		return result;
 	}
 
@@ -67,18 +62,19 @@ public class MineField {
 		Scanner in = new Scanner(System.in);
 		int size = 0;
 		int mines = 0;
-		
-		while(size <= 0 || size >= 15) {
+
+		while (size <= 0 || size >= 15) {
 			try {
 				System.out.print("Enter size: ");
 				size = in.nextInt();
-			} catch(Exception e) {}
+			} catch (Exception e) {
+			}
 		}
-		
+
 		mines = size / 2 + new Random().nextInt(size);
-		
+
 		in.close();
-		
-		System.out.println("\n" + toString(setMines(size, mines)));
+
+		System.out.println("\n" + getNewMineField(size, mines));
 	}
 }
